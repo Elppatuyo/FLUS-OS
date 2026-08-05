463b82d313fed8fbff3f0b7e3650da2fe83cf51e
Elppatuyo <Johanaeita@gmail.com>
2026-08-05 08:32:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+// Contraseña temporal
+const PASSWORD = "123";
 
+// Entrar al sistema
+function login() {
+    const input = document.getElementById("password").value;
+    const error = document.getElementById("error");
+
+    if (input === PASSWORD) {
+        document.getElementById("lockScreen").style.display = "none";
+        document.getElementById("desktop").style.display = "block";
+    } else {
+        error.textContent = "Contraseña incorrecta";
+    }
+}
+
+// Actualizar reloj
+function updateClock() {
+    const clock = document.getElementById("clock");
+
+    if (!clock) return;
+
+    const now = new Date();
+
+    const h = String(now.getHours()).padStart(2, "0");
+    const m = String(now.getMinutes()).padStart(2, "0");
+clock.textContent = `${h}:${m}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+// Botones temporales
+window.onload = () => {
+
+    document.getElementById("addGame").onclick = () => {
+        alert("En la siguiente parte agregaremos el sistema Add Games.");
+    };
+
+    document.getElementById("settings").onclick = () => {
+        alert("La configuración llegará en la siguiente parte.");
+    };
+
+};
