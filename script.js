65918d3cfb20fb65cb124962c7a303195e343c72
Elppatuyo <Johanaeita@gmail.com>
2026-08-05 22:11:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,86 @@
 const PASSWORD = "123";
 
-const lockScreen = document.getElementById("lockScreen");
+const login = document.getElementById("login");
 const desktop = document.getElementById("desktop");
-const loginBtn = document.getElementById("loginBtn");
-const passwordInput = document.getElementById("password");
-const loginError = document.getElementById("loginError");
 
-desktop.style.display = "none";
+const password = document.getElementById("password");
+const loginBtn = document.getElementById("loginBtn");
+const error = document.getElementById("error");
+
+const clock = document.getElementById("clock");
+
+const windowBox = document.getElementById("window");
+const closeWindow = document.getElementById("closeWindow");
 
 loginBtn.onclick = function () {
 
-    if (passwordInput.value === PASSWORD) {
+    if (password.value === PASSWORD) {
 
-        lockScreen.style.display = "none";
+        login.style.display = "none";
         desktop.style.display = "block";
 
     } else {
 
-        loginError.textContent = "Contraseña incorrecta";
+        error.innerHTML = "Contraseña incorrecta";
 
     }
 
 };
 
+password.addEventListener("keydown", function(e){
+
+    if(e.key === "Enter"){
+        loginBtn.click();
+    }
+
+});
+
 function updateClock(){
 
-    const clock = document.getElementById("taskbarClock");
+    const now = new Date();
 
-    const d = new Date();
-
-    let h = d.getHours();
-    let m = d.getMinutes();
+    let h = now.getHours();
+    let m = now.getMinutes();
 
     if(h < 10) h = "0" + h;
     if(m < 10) m = "0" + m;
 
-    clock.textContent = h + ":" + m;
+    clock.innerHTML = h + ":" + m;
 
 }
 
 updateClock();
 
-setInterval(updateClock,1000);8
-);
+setInterval(updateClock,1000);
+
+document.getElementById("settings").onclick = function(){
+
+    windowBox.style.display = "block";
+
+    document.getElementById("windowTitle").innerHTML =
+    "Configuración";
+
+    document.getElementById("frame").src =
+    "about:blank";
+
+};
+
+document.getElementById("addGames").onclick = function(){
+
+    windowBox.style.display = "block";
+
+    document.getElementById("windowTitle").innerHTML =
+    "Add Games";
+
+    document.getElementById("frame").src =
+    "about:blank";
+
+};
+
+closeWindow.onclick = function(){
+
+    windowBox.style.display = "none";
+
+    document.getElementById("frame").src = "";
+
+};
